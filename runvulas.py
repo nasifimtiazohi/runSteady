d9c02f7995cf557832a85a57ed16661d322039c5
nasifimtiazohi <nasifimtiaz88@gmail.com>
2020-09-12 22:25:09 -0400

    script for running vulas

--- runvulas.py
+++ runvulas.py
@@ -1,27 +1,53 @@
 import os
-
-os.chdir('/home/nasifimtiaz/Desktop/openmrs-module-fhir-1.20.0')
-
-
+import subprocess, shlex
 from datetime import datetime
 
-start = datetime.now()
+def get_projects():
+    projectPaths=[]
+    path = '/home/simtiaz/openmrsCopyRepos'
+    os.chdir(path)
+    
+    lines = subprocess.check_output(shlex.split('ls'), encoding='437').split('\n')[:-1]
+    for line in lines:
+        projectPaths.append(path+'/'+line+'/')
+    
+    assert len(projectPaths) == 43
+    return projectPaths 
 
-commands=[
-	'mvn -Dvulas compile vulas:app',
-	'export MAVEN_OPTS="-Xmx8g -Xms2g"',
-	'mvn -Dvulas compile vulas:a2c',
-	'mvn -Dvulas vulas:prepare-vulas-agent test vulas:upload -Dmaven.test.failure.ignore=true',
-	'mvn package',
-	'mvn -Dvulas vulas:instr',
-	'mvn -Dvulas vulas:t2c',
-	'mvn -Dvulas vulas:upload',
-	'mvn -Dvulas vulas:report'
-]
+    
 
-for c in commands:
-	os.system(c)
+def run(path):
+    project= path.split('/')[-1]
+    
+    commands=[
+        'mvn -Dvulas vulas:cleanSpace',
+        'mvn -Dvulas compile vulas:app',
+        'export MAVEN_OPTS="-Xmx8g -Xms2g"',
+        'mvn -Dvulas compile vulas:a2c',
+        'mvn -Dvulas vulas:prepare-vulas-agent test vulas:upload -Dmaven.test.failure.ignore=true',
+        'mvn package',
+        'mvn -Dvulas vulas:instr',
+        'mvn -Dvulas vulas:t2c',
+        'mvn -Dvulas vulas:upload',
+        'mvn -Dvulas vulas:report'
+    ]
+    
+    os.chdir(path)
+    
+    file = open('{}log.txt'.format(project),'w+')
+    
+    start = datetime.now()
+    print(datetime.now())
+    for c in commands:
+        os.system(c)
+        print(datetime.now())
+    end=datetime.now()
+    print(end-start)
+    
+    os.system('mv ./target/vulas/report/vulas-report.json /home/simtiaz/runSteady/{}-vulas.json'.format(project))
 
-end=datetime.now()
-
-print(start, end, start-end)
+if __name__=='__main__':
+    paths = get_projects()
+    for path in paths:
+        run(path)
+        break
